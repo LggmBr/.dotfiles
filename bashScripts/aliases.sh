ddfa38f9a01a034a40f6dc535373d486e8e20777
Luiz <137808191+LggmBr@users.noreply.github.com>
2024-06-03 10:57:02 -0300

    Added some pacman aliases

--- bashScripts/aliases.sh
+++ bashScripts/aliases.sh
@@ -2,5 +2,10 @@
 alias apti="sudo apt install"
 alias aptup="sudo apt update && sudo apt upgrade"
 
+# Pacman aliases
+alias paci="sudo pacman -S"
+alias pacup="sudo pacman -Syu"
+alias pacrm="sudo pacman -Rs"
+
 # Programs aliases
 alias vim="nvim"
