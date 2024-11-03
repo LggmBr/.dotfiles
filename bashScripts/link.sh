b4f080a96ee8dac527eda8ae92bf658118b57365
Luiz Gustavo Garibaldi Malikoski <137808191+LggmBr@users.noreply.github.com>
2024-11-03 17:53:42 -0300

    Modified some links and added a .dotfiles folder variable to avoid unwanted behavior

--- bashScripts/link.sh
+++ bashScripts/link.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+dotfiles=$HOME/.dotfiles
 
-ln -srf ./nvim $HOME/.config/nvim
-ln -srf ./starship.toml $HOME/.config/starship.toml
+ln -srf $dotfiles/nvim $HOME/.config/nvim
+ln -srf $dotfiles/starship.toml $HOME/.config/starship.toml
+
+sudo ln -srf $dotfiles/kanata/config.kbd /etc/kanata/config.kbd
