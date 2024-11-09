8f593ce825c644cd42547c93b15232a7826f7db4
Luiz Gustavo Garibaldi Malikoski <137808191+LggmBr@users.noreply.github.com>
2024-11-09 16:58:53 -0300

    Added Home-Manager

--- flake.nix
+++ flake.nix
@@ -20,7 +20,11 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ 
+          ./home.nix 
+          ./files.nix
+          ./services.nix
+        ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
--- home.nix
+++ home.nix
@@ -36,21 +36,6 @@
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
