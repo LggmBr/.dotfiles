7b057d68e814a5d79a6f9ea43c0c99e82e5ee9e5
Luiz Gustavo Garibaldi Malikoski <137808191+LggmBr@users.noreply.github.com>
2024-12-12 21:25:58 -0300

    Updated the dotfiles symlink script

--- bashScripts/link.sh
+++ bashScripts/link.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 dotfiles=$HOME/.dotfiles
 
-ln -srf $dotfiles/nvim $HOME/.config/nvim
-ln -srf $dotfiles/starship.toml $HOME/.config/starship.toml
+# Directory links:
+ln -srf $dotfiles/alacritty $HOME/.config/
+ln -srf $dotfiles/wlogout $HOME/.config/
+ln -srf $dotfiles/waybar $HOME/.config/
+ln -srf $dotfiles/nvim $HOME/.config/
+ln -srf $dotfiles/hypr $HOME/.config/
 
+# Single file links:
+ln -srf $dotfiles/starship.toml $HOME/.config/starship.toml
+ln -srf $dotfiles/.bashrc $HOME/.bashrc
+
+# Links that require root privileges:
 sudo ln -srf $dotfiles/kanata/config.kbd /etc/kanata/config.kbd
