d8716a2d72b1319587dc752753871a9c75d06823
Luiz Gustavo Garibaldi Malikoski <137808191+LggmBr@users.noreply.github.com>
2024-12-12 22:52:06 -0300

    Splitted my neovim configuration into multiple files for easy maintenance

---
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+	"catppuccin/nvim",
+	priority = 1000, -- Make sure to load this before all the other start plugins.
+	init = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
+		-- Load the colorscheme here.
+		-- Like many other themes, this one has different styles, and you could load
+		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+		vim.cmd.colorscheme("catppuccin-mocha")
+
+		-- You can configure highlights by doing something like:
+		vim.cmd.hi("Comment gui=none")
+	end,
+}
