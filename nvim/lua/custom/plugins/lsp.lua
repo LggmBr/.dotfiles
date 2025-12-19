355bd70fd43891b3c380803b972d2b290feee2cf
LggmBr <lggm@lgc.com>
2025-12-19 19:30:13 -0300

    Added new LSP server for mason to ensure they are installed.

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -226,7 +226,9 @@ return {
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
+				"lua_ls",
+				"gopls",
+				"stylua",
 				"goimports-reviser",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
